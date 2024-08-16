eefca70e0fa691a6e1a7ad8ba032d3f9719b4b75
habib ulla <habibulla1278@gmail.com>
2024-08-16 10:48:39 +0600

    search function added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+const port = process.env.PORT || 3000;
 
+require('dotenv').config()
+app.use(cors({
+    origin: [
+        "https://panda-9805f.web.app",
+        'http://localhost:5173',
+    ],
+    credentials: true
+}));
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -21,6 +31,46 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
+        const allProduct = client.db("panda").collection("products")
+        app.get("/products", async (req, res) => {
+            try {
+                const { search } = req.query;
+                let products;
+
+                if (search) {
+                    products = await allProduct.find({ name: new RegExp(search, 'i') }).toArray();
+                    return res.send(products); // Use return to exit the function after sending response
+                }
+
+                products = await allProduct.find().toArray();
+                return res.send(products); // Use return to ensure no further code is executed
+            } catch (error) {
+                console.error(error);
+                return res.status(500).send("Internal Server Error"); // Handle errors properly
+            }
+        });
+
+
+        app.get("/shoe", async (req, res) => {
+            const result = await allProduct.find({ category: "Shoe" }).toArray()
+
+            res.send(result)
+        })
+        app.get("/watch", async (req, res) => {
+            const result = await allProduct.find({ category: "Watch" }).toArray()
+
+            res.send(result)
+        })
+        app.get("/tv", async (req, res) => {
+            const result = await allProduct.find({ category: "TV" }).toArray()
+
+            res.send(result)
+        })
+        app.get("/bag", async (req, res) => {
+            const result = await allProduct.find({ category: "Bag" }).toArray()
+
+            res.send(result)
+        })
         app.get('/', (req, res) => {
             res.send('Hello World!')
         })
